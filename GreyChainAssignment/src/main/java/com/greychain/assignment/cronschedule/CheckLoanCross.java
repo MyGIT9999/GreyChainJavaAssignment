bc98feafad1200cbbce7b0e066018e1b766a4844
Jay131299 <57975503+Jay131299@users.noreply.github.com>
2023-07-21 10:37:37 +0530

    Validation for loan crossed due date

--- GreyChainAssignment/src/main/java/com/greychain/assignment/cronschedule/CheckLoanCross.java
+++ GreyChainAssignment/src/main/java/com/greychain/assignment/cronschedule/CheckLoanCross.java
@@ -5,16 +5,11 @@ import com.greychain.assignment.service.LoanService;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.text.SimpleDateFormat;
 import java.util.logging.Logger;
 
-@Component
+
 public class CheckLoanCross {
     private static final Logger log = (Logger) LoggerFactory.getLogger(CheckLoanCross.class);
-
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Autowired
     LoanService loanService;
